38d32441d30a9e8fec8734fb8e5d2e851be821a6
Jorge Wong <jorge.wong@kiubix.com>
2023-01-19 22:15:15 -0700

    Agregando endpoint para webhook

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { createConnection } from "mysql2/promise";
 
+//import library to make post requests
+
+//use body parser to parse json
 
 const app = express();
-const connection = await createConnection({
-	host: "db",
-	user: "root",
-	password: "example",
-	database: "aws_example",
-});
+app.use(bodyParser.json());
+// const connection = await createConnection({
+// 	host: "db",
+// 	user: "root",
+// 	password: "example",
+// 	database: "aws_example",
+// });
 
 app.get("/", async (req, res) => {
+	// const [rows, fields] = await connection.execute("SELECT * FROM `users`");
 
-  const [rows, fields] = await connection.execute("SELECT * FROM `users`");
+	// res.json(rows);
 
-	res.json(rows);
+	res.send("Hello World from here, I am running on port 8000");
+});
 
-  // res.send("Hello World from here, I am running on port 8000, xd   AA");
+app.post("/get-data", async (req, res) => {
+	const { body } = req;
+	return res.send({ body });
 });
 
 app.listen(8000, () => {
